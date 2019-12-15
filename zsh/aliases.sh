a3493dda0f45ab867583af9ccb6aed8dbe99d480
fltonii <tonialjoao97@gmail.com>
2019-12-15 12:14:34 -0300

    adding zsh configs

---
+++ zsh/aliases.sh
@@ -0,0 +1,4 @@
+alias zshconfig="vim ~/.zshrc"
+alias ohmyzsh="vim ~/.oh-my-zsh"
+alias py="python"
+alias rdr="cd ~/exp/real-life/rdr/frontend
---
+++ zsh/install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+DIR="$(dirname "$(which "$0")")"
+
+install_zsh() {
+  if command -v zsh > /dev/null ; then 
+    echo "== zsh detected"
+    echo "zsh current version: "
+  else
+    yay -Sy --noconfirm zsh
+  fi
+  echo zsh --version
+}
+
+install_oh_my_zsh() {
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  # install powerline
+  if pip --version > /dev/null ; then
+    pip install powerline-status
+  else
+    python "$(curl -fsSL https://bootstrap.pypa.io/get-pip.py)"
+    install_oh_my_zsh
+  fi
+}
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlight
+
+install_zsh
+install_oh_my_zsh
